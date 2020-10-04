74b31f6119febb54f196d647bf7cb0378befc4e1
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-04 18:46:19 +0900

    common page setting

---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+import { waitForElementToBeRemoved } from '@testing-library/react';
+import React from 'react';
+
+const Footer = () => (
+    <div className="footer">
+        ⓒ JH React Practice
+    </div>
+)
+
+export default Footer;
