0a960e4d9622aa97fa0ed7efca23458cd4fb14a5
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-04 19:23:48 +0900

    json(dummy data) && axios setting

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from "../component/Header";
 import Gnb from "../component/Gnb";
 import Footer from "../component/Footer";
+import WebtoonList from "../component/WebtoonList";
 
 class Main extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            day : 'mon', //default Monday
+            webtoonList : [] //init data
+        };
+    }
+
+        componentDidMount() {
+            this._getList();
+        }
+
+        _getList() {
+            // webtoon_list get
+            const apiUrl = 'dummy/webtoon_list.json';
+
+            axios.get(apiUrl)
+                .then(data => {
+                    this.setState({
+                        webtoonList : data.data.webtoonList
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                }) 
+        }
+
+
     render () {
         return (
             <div>
                 <Header/>
                 <Gnb />
-                Main
+                {
+                    this.state.webtoonList.length > 0 ? (
+                        <WebtoonList list={
+                            this.state.webtoonList.filter(webtoon => (
+                                webtoon.day === this.state.day
+                            ))
+                        } />
+                    ) : (
+                        <span>
+                            LODING...
+                        </span>
+                    )
+                }
                 <Footer />
             </div>
         )
