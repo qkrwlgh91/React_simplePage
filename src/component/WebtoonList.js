1c2d93c4a02e0e6400e1f6064d1d4c40106d7fbb
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-04 20:01:08 +0900

    webtoon detail page

--- src/component/WebtoonList.js
+++ src/component/WebtoonList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const WebtoonList = (props) => {
     return (
         <ul className="list_webtoon">
             { props.list.map((webtoon, index) => (
                 <li key={index}>
-                    <Link to="/" className="link_webtoon">
+                    <Link to={`/webtoon/${webtoon.id}`} className="link_webtoon">
                         <img src={webtoon.thumbnail} className="img_webtoon" alt={webtoon.title} />
                         <div className="info_webtoon">
                             <strong className="tit_webtoon">
@@ -16,7 +16,7 @@ const WebtoonList = (props) => {
                         </div>
                     </Link>
                 </li>
-            ))}
+            )) }
         </ul>
     )
 }
--- src/container/Main.js
+++ src/container/Main.js
@@ -6,55 +6,54 @@ import Gnb from "../component/Gnb";
 import Footer from "../component/Footer";
 import WebtoonList from "../component/WebtoonList";
 
-class Main extends Component {
-
-    constructor(props) {
+class Main extends Component{
+    constructor(props){
         super(props);
 
         this.state = {
-            day : 'mon', //default Monday
-            webtoonList : [] //init data
+            day : 'mon', //디폴트로 월요일
+            webtoonList : [] //초기 리스트는 비어있습니다.
         };
     }
 
-        componentDidMount() {
-            this._getList();
-        }
+    componentDidMount(){
+        this._getList();
+    }
 
-        _getList() {
-            // webtoon_list get
-            const apiUrl = 'dummy/webtoon_list.json';
+    _getList(){
+        //webtoon_list를 가지고 옵니다.
+        const apiUrl = '/dummy/webtoon_list.json';
 
-            axios.get(apiUrl)
-                .then(data => {
-                    this.setState({
-                        webtoonList : data.data.webtoonList
-                    });
-                })
-                .catch(error => {
-                    console.log(error);
-                }) 
-        }
+        axios.get(apiUrl)
+            .then(data => {
+                //가지고 온 리스트를 state에 저장합니다.
+                this.setState({
+                    webtoonList : data.data.webtoonList
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-
-    render () {
+    render(){
         return (
             <div>
-                <Header/>
+                <Header />
                 <Gnb />
-                {
-                    this.state.webtoonList.length > 0 ? (
-                        <WebtoonList list={
-                            this.state.webtoonList.filter(webtoon => (
-                                webtoon.day === this.state.day
-                            ))
-                        } />
-                    ) : (
-                        <span>
-                            LODING...
-                        </span>
-                    )
-                }
+
+                { this.state.webtoonList.length > 0 ? (
+                    <WebtoonList list={
+                        this.state.webtoonList.filter(webtoon => (
+                            webtoon.day === this.state.day
+                        ))
+                    } />
+                ) : (
+                    <span>
+                        LOADING...
+                    </span>
+                )}
+
                 <Footer />
             </div>
         )
